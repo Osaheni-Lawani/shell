dea1aab2d0c0e0c86e5b903132c910e121ca9e7a
Osaheni-Lawani <lawkins2020@gmail.com>
2026-03-03 02:23:51 -0500

    Update ls command to specify directory path

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -36,7 +36,7 @@ unzip -q rawdata.zip
 
 # 3. List the contents of the ./data/raw directory
 
-ls
+ls ./data/raw
 
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
